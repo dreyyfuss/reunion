1d45809e77303182b056fc32477e498762d7d872
Anthony Alikah <a.o.alikah@gmail.com>
2025-07-03 12:07:35 +0100

    Allow all hosts fix

--- reunion/settings.py
+++ reunion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=p_t4ug!oq0%%ju=)dq^ep9qj**sp_@fv&-!3m6d4pdtu&md^%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
